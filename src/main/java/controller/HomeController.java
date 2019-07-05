061a0199ecc1252b08f8727b4436f61cb3ae0b49
e.zhbanov <thatgenius1@gmail.com>
2019-07-05 11:15:30 +0500

    first commit

---
+++ src/main/java/controller/HomeController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @RequestMapping(value = "/")
+    public String list() {
+        return "main";
+    }
+}
